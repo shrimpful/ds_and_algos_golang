f1618f4fea423d7ed3497bfdc42b844350a12490
Maxim Rudenko <Rudenko.Maksim@wildberries.ru>
2021-01-09 22:33:40 +0300

    added some data structures

---
+++ chapter1/tuples.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+//gets the power series of integer a and returns tuple of square of a
+// and cube of a
+func powerSeries(a int) (int, int) {
+	return a*a,a*a*a
+}
+
+func main() {
+	var square int
+	var cube int
+
+	square,cube=powerSeries(3)
+	fmt.Println("Square:",square,"\nCube:",cube)
+}
