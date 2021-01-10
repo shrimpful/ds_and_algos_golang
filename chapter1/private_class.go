48f7db0cf5d621676f37db3b28e4aa2ec4225837
shrimpful <Rudenko.Maksim@wildberries.ru>
2021-01-10 20:47:18 +0300

    added others structural patterns

---
+++ chapter1/private_class.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type AccountDetails struct {
+	id string
+	accountType string
+}
+
+type Account struct {
+	details *AccountDetails
+	CustomerName string
+}
+
+func (account *Account) setDetails(id, accountType string) {
+	account.details=&AccountDetails{id,accountType}
+}
+
+func (account *Account) getId() string {
+	return account.details.id
+}
+
+func (account *Account) getAccountType() string {
+	return account.details.accountType
+}
+
+func main() {
+	var account *Account=&Account{CustomerName: "John Smith"}
+	account.setDetails("4532","current")
+	jsonAccount,_:=json.Marshal(account)
+	fmt.Println("Private Class hidden",string(jsonAccount))
+	fmt.Println("Account Id",account.getId())
+	fmt.Println("Account Type",account.getAccountType())
+}
