d814a3d049c2d06d33de64351a84361f1ffd0945
shrimpful <Rudenko.Maksim@wildberries.ru>
2021-01-19 23:42:32 +0300

    added chapter7

---
+++ chapter7/thue_morse.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func ThueMorseSequence(buffer *bytes.Buffer) {
+	var b int
+	var currLength int
+	var currBytes []byte
+	for b, currLength, currBytes = 0, buffer.Len(), buffer.Bytes(); b < currLength; b++ {
+		if currBytes[b] == '1' {
+			buffer.WriteByte('0')
+		} else {
+			buffer.WriteByte('1')
+		}
+	}
+}
+
+func main() {
+	var buffer bytes.Buffer
+	buffer.WriteByte('0')
+	fmt.Println(buffer.String())
+	var i int
+	for i = 2; i <= 7; i++ {
+		ThueMorseSequence(&buffer)
+		fmt.Println(buffer.String())
+	}
+}
